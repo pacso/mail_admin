d3b82e4e4fd0afc7d69fd71119651fe4b3e9b7c6
Jon Pascoe <jon.pascoe@me.com>
2013-03-07 09:34:16 +0000

    Created empty Rails 3.2.12 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailAdmin::Application.config.secret_token = '6ba817a77ca63298d56cc3c79d8349cee3d9a9785229742ec0a8dfd7b2dd5c26d496cdc641ae80884fdbcd4ae5419840380db2aad88d8b9925373b695716cc7a'
