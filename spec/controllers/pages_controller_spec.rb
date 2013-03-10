d0a77c9dea7d3e66c2324a74847533ddcb7d1bdc
Jon Pascoe <jon.pascoe@me.com>
2013-03-10 15:44:23 +0000

    Reordered tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,46 +3,58 @@ require 'spec_helper'
 describe PagesController do
   
   describe "GET #index" do
+    let!(:domain1) { create :domain }
+    let!(:domain2) { create :domain }
     let(:get_index) { -> { get :index} }
     
     it "requires you to be signed in" do
-      get_index
+      get :index
       expect(response).to require_sign_in
     end
     
-    describe "when logged in" do
-      let!(:domain1) { create :domain }
-      let!(:domain2) { create :domain }
+    context "as a standard user" do
       let(:mailbox) { create :mailbox, domain: domain1 }
-      before(:each) do
-        set_current_mailbox(mailbox)
-      end
+      before { set_current_mailbox mailbox }
       
       it "renders the index view" do
         get :index
-        expect(response).to render_template(:index)
+        expect(response).to render_template :index
       end
       
-      context "with a standard account" do
-        it "does not assign a list of domains" do
-          # expect get_index.to
-          expect(assigns(:domains)).to be_nil
-        end
+      it "does not assign a list of domains" do
+        get :index
+        expect(assigns(:domains)).to be_nil
       end
+    end
+    
+    context "as a domain_admin" do
+      let(:mailbox) { create :domain_admin_mailbox, domain: domain1 }
+      before { set_current_mailbox mailbox }
       
-      context "as a domain_admin" do
-        let(:mailbox) { create :domain_admin_mailbox, domain: domain1 }
-      end
-      
-      context "as a site_admin" do
-        let(:mailbox) { create :site_admin_mailbox, domain: domain1 }
-        
-        it "assigns a list of domains" do
-          get :index
-          expect(assigns(:domains)).to match_array [domain1, domain2]
-        end
+      it "renders the index view" do
+        get :index
+        expect(response).to render_template :index
       end
 
+      it "does not assign a list of domains" do
+        get :index
+        expect(assigns(:domains)).to be_nil
+      end
+    end
+    
+    context "as a site_admin" do
+      let(:mailbox) { create :site_admin_mailbox, domain: domain1 }
+      before { set_current_mailbox mailbox }
+      
+      it "renders the index view" do
+        get :index
+        expect(response).to render_template :index
+      end
+
+      it "assigns a list of domains" do
+        get :index
+        expect(assigns(:domains)).to match_array [domain1, domain2]
+      end
     end
   end
 end
