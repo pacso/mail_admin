40246f0125e7d0883ec92879185d907c4734a6eb
Jon Pascoe <jon.pascoe@me.com>
2013-03-10 16:07:34 +0000

    Moved common tests into a shared_examples block

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,60 +1,59 @@
 require 'spec_helper'
 
 describe PagesController do
+  let!(:domain1) { create :domain }
+  let!(:domain2) { create :domain }
   
-  describe "GET #index" do
-    let!(:domain1) { create :domain }
-    let!(:domain2) { create :domain }
-    let(:get_index) { -> { get :index} }
-    
-    it "requires you to be signed in" do
+  shared_examples("standard template rendering") do
+    describe "GET #index" do
+      it "renders the index view" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
+  
+  describe "when signed out" do
+    it "requires you to sign in" do
       get :index
       expect(response).to require_sign_in
     end
-    
-    context "as a standard user" do
-      let(:mailbox) { create :mailbox, domain: domain1 }
-      before { set_current_mailbox mailbox }
-      
-      it "renders the index view" do
-        get :index
-        expect(response).to render_template :index
-      end
-      
-      it "does not assign a list of domains" do
-        get :index
-        expect(assigns(:domains)).to be_nil
-      end
+  end
+  
+  describe "when signed in" do
+    before(:each) do
+      set_current_mailbox mailbox
     end
     
-    context "as a domain_admin" do
-      let(:mailbox) { create :domain_admin_mailbox, domain: domain1 }
-      before { set_current_mailbox mailbox }
-      
-      it "renders the index view" do
-        get :index
-        expect(response).to render_template :index
-      end
+    describe "as a standard user" do
+      let(:mailbox) { create(:mailbox, domain: domain1) }
+      it_behaves_like "standard template rendering"
 
       it "does not assign a list of domains" do
         get :index
         expect(assigns(:domains)).to be_nil
       end
     end
-    
-    context "as a site_admin" do
-      let(:mailbox) { create :site_admin_mailbox, domain: domain1 }
-      before { set_current_mailbox mailbox }
-      
-      it "renders the index view" do
+
+    describe "as a domain admin" do
+      let(:mailbox) { create(:domain_admin_mailbox, domain: domain1) }
+      it_behaves_like "standard template rendering"
+
+      it "does not assign a list of domains" do
         get :index
-        expect(response).to render_template :index
+        expect(assigns(:domains)).to be_nil
       end
+    end
+
+    describe "as a site admin" do
+      let(:mailbox) { create(:site_admin_mailbox, domain: domain1) }
+      it_behaves_like "standard template rendering"
 
       it "assigns a list of domains" do
         get :index
         expect(assigns(:domains)).to match_array [domain1, domain2]
       end
     end
+    
   end
 end
