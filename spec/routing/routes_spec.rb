2a5e61dcbad4a5ec02885bf9c0e8a63cf53dde1d
Jon Pascoe <jon.pascoe@me.com>
2013-03-11 23:37:45 +0000

    Added route tests for the other static routes

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -1,12 +1,46 @@
 require 'spec_helper'
 
 describe "Routes" do
-  describe "Homepage" do
-    it "routes / to pages#index" do
-      { get: '/' }.should route_to(
-        controller: "pages",
-        action: "index"
-      )
-    end
+
+  it "get / to pages#index" do
+    expect(get: '/').to route_to(
+      controller: "pages",
+      action: "index"
+    )
+  end
+  
+  it "get /sign_in to sessions#new" do
+    expect(get: '/sign_in').to route_to(
+      controller: 'sessions',
+      action: 'new'
+    )
+  end
+  
+  it "post /sign_in to sessions#create" do
+    expect(post: '/sign_in').to route_to(
+      controller: 'sessions',
+      action: 'create'
+    )
+  end
+  
+  it "delete /sign_out to sessions#destroy" do
+    expect(delete: '/sign_out').to route_to(
+      controller: 'sessions',
+      action: 'destroy'
+    )
+  end
+  
+  it "get /change_password to passwords#new" do
+    expect(get: '/change_password').to route_to(
+      controller: 'passwords',
+      action: 'new'
+    )
+  end
+  
+  it "put /change_password to passwords#update" do
+    expect(put: '/change_password').to route_to(
+      controller: 'passwords',
+      action: 'update'
+    )
   end
 end
